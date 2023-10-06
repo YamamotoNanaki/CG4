4ae89e53372ceb42a9d9fc588b61f343e1e53889
虹輝 山本 <nanaki0203.y@gmail.com>
2023-10-06 14:16:07 +0900

    一旦警告レベルを4に戻した

--- Engine/Base/Warning.h
+++ Engine/Base/Warning.h
@@ -7,17 +7,17 @@
 
 #define WarningDisabledAll \
 WarningDisabledNum(26812) \
-WarningDisabledNum(2220) \
-WarningDisabledNum(2143) \
-WarningDisabledNum(2447) \
-WarningDisabledNum(4430) \
-WarningDisabledNum(2365) \
-WarningDisabledNum(3646) \
-WarningDisabledNum(2040) \
-WarningDisabledNum(2065) \
-WarningDisabledNum(2144) \
-WarningDisabledNum(3861) \
-WarningDisabledNum(1003) \
+//WarningDisabledNum(2220) \
+//WarningDisabledNum(2143) \
+//WarningDisabledNum(2447) \
+//WarningDisabledNum(4430) \
+//WarningDisabledNum(2365) \
+//WarningDisabledNum(3646) \
+//WarningDisabledNum(2040) \
+//WarningDisabledNum(2065) \
+//WarningDisabledNum(2144) \
+//WarningDisabledNum(3861) \
+//WarningDisabledNum(1003) \
 
 #define WarningDisabled \
 WarningDisabledPush \
